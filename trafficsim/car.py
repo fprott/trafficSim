4f4c23d716353501fa1d00306db454864f93e1f4
Balog <balog270891@yahoo.com>
2017-05-31 12:06:20 +0200

    pew pew Cars/ Car_size update

--- trafficsim/car.py
+++ trafficsim/car.py
@@ -59,6 +59,20 @@ class Car:
         else:
             return self.max_a
 
+        def simple_car_collision(self, car_2):
+            """Kollisionscheck das schnell ist aber nicht exakt"""
+            dist = math.hypot(self.pos.x - car_2.pos.x, self.pos.y - car_2.pos.y)
+            diag_1 = math.sqrt((self.size.get_width() ** 2) + (self.size.get_length() ** 2))
+            diag_2 = math.sqrt((car_2.size.get_width() ** 2) + (car_2.size.get_length() ** 2))
+            if dist <= 0.5 * (diag_1 + diag_2):
+                return True
+            else:
+                return False
+
+            def exact_car_collision(self, car_2):
+                """Kollision über Separating Axis Theorem ist exact aber langsam und funktioniert noch nicht richtig :D"""
+                return 0
+
     #def get_next_car(self, dt, new_a):
     #    """new_a ist ein Wert aus der Liste a_values"""
     #    new_v = self.v + new_a * dt  # wir erechnen die neue geschwindigkeit
@@ -71,11 +85,20 @@ class Car:
         return (str(self.id) + " " + str(self.a))
 
 class CarSize():
-    def __init__(self):
-        pass  # TODO implement me
+    """die größere Zahl ist immer length"""
+    def __init__(self, width, length):
+        if length >= width :
+            self.width = width
+            self.length = length
+        else:
+            self.width=length
+            self.length=width
 
-    def get_size(self):
-        pass  # TODO implement me
+    def get_width(self):
+        return self.width
+
+    def get_length(self):
+        return self.length
 
 class Route():
     def __init__(self):
@@ -147,3 +170,12 @@ class Route():
     def get_angle_of_pos(self, pos):
         """Gibt den Winkel zurück so als ob das Auto von Start zu Ende geht"""
         return angle
+
+
+
+
+
+
+
+
+
