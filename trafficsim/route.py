d78ebbb46b09552418af96d2925e6c1285ed1b65
theChaoS <florianprott@gmail.com>
2017-06-08 15:09:10 +0200

    Bugfix in route und beispiel

--- trafficsim/route.py
+++ trafficsim/route.py
@@ -33,7 +33,7 @@ class Strecke:
 
 class Route:
     def __init__(self, strecke, dStep):
-        self.strecke=strecke # Die Route bassiert auf einer Strecke. Wenn man die Route ändert so ändert man auch die Strecke und umgekehert !
+        self.strecke = strecke # Die Route bassiert auf einer Strecke. Wenn man die Route ändert so ändert man auch die Strecke und umgekehert !
         self.points = self._get_path_as_many_points(dStep)
         self.positon= strecke.getStartPoint()
         self._dStep=dStep
@@ -45,7 +45,7 @@ class Route:
         :param dStep: Punkt genauigkeit
         :return:
         """
-        roh_daten = self.strecke
+        roh_daten = self.strecke.points
         x_roh=[]
         y_roh=[]
         for p in roh_daten:
@@ -55,26 +55,39 @@ class Route:
 
         y_points = numpy.interp(x_range, x_roh, y_roh)
         points=[]
-        for i in range(0,len(y_points))
+        for i in range(0,len(y_points)):
             points.append(mathe.Point(y_points[i],x_range[i]))
         return points
 
     def get_new_pos(self, l): # ich gehe davon aus das sich die Einheit nicht ändert !
         """Verändert die Position um den Abstand l. l ist t*v"""
         k=1/self._dStep
-        self.point_iterator+=k
+        self.point_iterator+=int(k)
+        return self.points[self.point_iterator]
+
+    def get_current_pos(self):
         return self.points[self.point_iterator]
 
     def traveled_distance_on_route(self):
         """"Zurückgelegter Weg auf der Route d.h. wie weit wir schon gefahren sind"""
         k = 1 / self._dStep
-        return (len(self.points)-self.point_iterator)*k
+        return self.point_iterator*k
 
     def percent_of_route_still_to_travel(self):
         """Wie viel Prozent der Route noch zurückgelegt werden müssen wobei 0 Prozent heißt das wir angekommen sind und 100 Prozent das wir am Start sind"""
-        return 100-(100*self.point_iterator/len(self.points))
+        return 100-(100*self.point_iterator/(len(self.points)))
 
     def get_angle_of_pos(self, pos):
         """Gibt den Winkel zurück so als ob das Auto von Start zu Ende geht"""
     #    return angle
 
+# myStrecke = Strecke([mathe.Point(0.0,0.0),mathe.Point(1.0,2.0),mathe.Point(2.0,4.0),mathe.Point(3.0,8.0)])
+# myRoute = Route(myStrecke,0.1)
+# print(myRoute.get_current_pos())
+# print(myRoute.percent_of_route_still_to_travel())
+# print(myRoute.get_new_pos(1))
+# print(myRoute.percent_of_route_still_to_travel())
+# print(myRoute.get_new_pos(1))
+# print(myRoute.percent_of_route_still_to_travel())
+# print(myRoute.get_new_pos(1))
+# print(myRoute.percent_of_route_still_to_travel())
