7de2d7a1d325525fd720c7f3033f93bbcb09e14b
theChaoS <florianprott@gmail.com>
2017-06-11 21:11:53 +0200

    Bugfix in route und beispiel

--- trafficsim/car.py
+++ trafficsim/car.py
@@ -1,8 +1,9 @@
 from mathe import *
+from route import *
 
 class Car:
     """Car Klasse. Diese Klasse bietet die Grundlage aller Auto Objekte und soll nach möglichkeit geerbt werden"""
-    def __init__(self, id, a_max, a_min, v_max, v_min, v, a, start_pos, car_size):
+    def __init__(self, id, route, a_max, a_min, v_max, v_min, v, a, car_size):
         self.id=id
         self.a_min = a_min
         self.a_max = a_max
@@ -11,8 +12,8 @@ class Car:
 
         self.a = a
         self.v = v #
-        self.pos = start_pos
-        self.route = Route(start_pos) # TODO Implement me :D
+        self.pos = route.get_current_pos()
+        self.route = route
         self.size = car_size
 
     def _set_v(self, new_v):
@@ -22,10 +23,10 @@ class Car:
             self.v = self.max_v
 
     def _set_a(self, new_a):
-        if (new_a <= self.max_a):
+        if (new_a <= self.a_max):
             self.a = new_a
         else:
-            self.a = self.max_a
+            self.a = self.a_max
         pass
 
     def get_pos(self):
@@ -51,13 +52,13 @@ class Car:
         Gibt eine legale Beschleunigung abhängig von einer Beschleunigunsänderung da an. Beachtet die minimal und maximal Beschleunigung.
         :return:
         """
-        if (self.a+da <= self.max_a): # wenn die neue beschleunigung kleiner als max beschleunigung
-            if(self.a+da >= self.min_a): # und wenn die (möglicherweise negative) beschleunigung größer als min beschleunigung
-                return self.a+da;
+        if (self.a+da <= self.a_max): # wenn die neue beschleunigung kleiner als max beschleunigung
+            if(self.a+da >= self.a_min): # und wenn die (möglicherweise negative) beschleunigung größer als min beschleunigung
+                return self.a+da
             else:
-                return self.min_a
+                return self.a_min
         else:
-            return self.max_a
+            return self.a_max
 
         def simple_car_collision(self, car_2):
             """Kollisionscheck das schnell ist aber nicht exakt"""
@@ -94,8 +95,8 @@ def check_collision(cars):
         for car_2 in cars:
             if(car_1 != car_2):
                 # der folgende Algo ist abgeschrieben, ka ob das funktioniert
-                dist = math.hypot(self.pos.x - car_2.pos.x, self.pos.y - car_2.pos.y)
-                diag_1 = math.sqrt((self.size.get_width() ** 2) + (self.size.get_length() ** 2))
+                dist = math.hypot(car_1.pos.x - car_2.pos.x, car_1.pos.y - car_2.pos.y)
+                diag_1 = math.sqrt((car_1.size.get_width() ** 2) + (car_1.size.get_length() ** 2))
                 diag_2 = math.sqrt((car_2.size.get_width() ** 2) + (car_2.size.get_length() ** 2))
                 if dist <= 0.5 * (diag_1 + diag_2):
                     return True
