997dac45d4fd7cd74acb381629ca308a529abe75
ziheng84 <ziheng.wang93@gmail.com>
2017-06-25 22:06:49 +0200

    updated

--- tests/test_wang.py
+++ tests/test_wang.py
@@ -1,8 +1,9 @@
 import sys
 import math
 sys.path.append("..")
-from trafficsim.route import *
+from route import *
 import unittest
+from parameter import *
 
 
 
@@ -16,13 +17,15 @@ class RoutenTest(unittest.TestCase):
         #print(Route1.point_iterator)
         print(Route1.traveled_distance_on_route())
         print(Route1.percent_of_route_still_to_travel())
+        print(Strassen_Nets[1][1])
+        print(Route1.get_angle_from_start(1000))
         #print(Route1.get_new_pos(-500))
-        print(Route1.get_new_pos_from_start(50))
+        #print(Route1.get_new_pos_from_start(50))
         #print(Route1.get_current_pos())
-        print(Route1.traveled_distance_on_route())
-        print(Route1.percent_of_route_still_to_travel())
-        print(Route1.get_percentage_from_start(100))
-        print(Route1.get_percentage_from_start(600))
+        #print(Route1.traveled_distance_on_route())
+        #print(Route1.percent_of_route_still_to_travel())
+        #print(Route1.get_percentage_from_start(100))
+        #print(Route1.get_percentage_from_start(600))
 
 
 
