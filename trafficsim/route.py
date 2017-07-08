3b82bc2f0e9a813e60a8426eb5e052daff55645a
theChaoS <florianprott@gmail.com>
2017-07-08 17:01:59 +0200

    mein teil läuft

--- trafficsim/route.py
+++ trafficsim/route.py
@@ -9,7 +9,7 @@ from mathe import *
 
 from parameter import *
 # Wangs Klasse wurde hierher verschoben !
-# getested, geht halbwegs XD
+
 
 #UPDATE 25/06/2017
 #get_next_pos input a negative Length
@@ -18,11 +18,10 @@ from parameter import *
 
 
 class Route():
-    def __init__(self,points,width, accur = 1):
+    def __init__(self,points,width):
         self.points = points  # The points to define the street
         self.width = width
-        self.accur = accur
-        self.routepoints = self.get_route(self.points, self.width , self.accur)
+        self.routepoints = self.get_route(self.points, self.width)
         self.point_iterator = 0
         self.routelength= self.route_length()
 
@@ -75,7 +74,7 @@ class Route():
         #print(new_p)
         return new_p
 
-    def get_route(self,points,width,accur):
+    def get_route(self,points,width):
         #from matplotlib import pyplot as plt
 
         #points = self.points
@@ -85,13 +84,9 @@ class Route():
         n = len(points)
         new_p = self.get_key_points(points,width)
         m = len(new_p)
-        s = math_Kurve()
 
-        kpoints = [new_p[0],points[0]]
-        numPoint = calculate_length(kpoints)
-        # print("numPoint is %r" % numPoint)
-        x, y = s.Bezier_Kurve(points=kpoints, nTimes=int(numPoint/accur))
-        # x,y = s.Bezier_Kurve(points=[new_p[0],points[0]],nTimes=numPoint/10)
+        s = math_Kurve()
+        x,y = s.Bezier_Kurve(points=[new_p[0],points[0]])
         x = list(x)
         y = list(y)
         y.pop()
@@ -99,9 +94,7 @@ class Route():
 
         for i in range(1,n-1):
             kpoints=[new_p[(2*i - 1)], points[i], new_p[(2*i-2)]]
-            numPoint = calculate_length(kpoints)
-            # print("numPoint is %r" % numPoint)
-            x_new, y_new = s.Bezier_Kurve(points=kpoints,nTimes=int(numPoint/accur)) # Ablenkung
+            x_new, y_new = s.Bezier_Kurve(points=kpoints) # Ablenkung
             x_new = list(x_new)
             y_new = list(y_new)
             x_new.pop()
@@ -109,22 +102,15 @@ class Route():
             x = list(x)+x_new
             y = list(y)+y_new
             lpoints=[new_p[2*i],new_p[(2*i-1)]]
-            numPoint = calculate_length(lpoints)
-            # print("numPoint is %r" % numPoint)
-            x_new, y_new = s.Bezier_Kurve(points=lpoints,nTimes=int(numPoint/accur))
-            # x_new, y_new = s.Bezier_Kurve(points=lpoints) #Gerade Linien
+            x_new, y_new = s.Bezier_Kurve(points=lpoints) #Gerade Linien
             x_new = list(x_new)
             y_new = list(y_new)
-            # x_new.pop()
-            # y_new.pop()
+            x_new.pop()
+            y_new.pop()
             x = list(x) + x_new
             y = list(y) + y_new
 
-        kpoints = [points[n-1],new_p[2*(n-2)]]
-        numPoint = calculate_length(kpoints)
-        # print("numPoint is %r" % numPoint)
-        x_final, y_final = s.Bezier_Kurve(points=kpoints, nTimes=int(numPoint/accur))
-        # x_final,y_final=s.Bezier_Kurve(points=[points[n-1],new_p[2*(n-2)]])
+        x_final,y_final=s.Bezier_Kurve(points=[points[n-1],new_p[2*(n-2)]])
         x_final = list(x_final)
         y_final = list(y_final)
         #x_final.pop()
@@ -406,18 +392,114 @@ class Route():
 
 #****************Beispiel*************************
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
 
 # Route1=Route([(500,1),(1000,1)],width=1)
 # print(Route1.get_new_pos(100))
 # print(Route1.traveled_distance_on_route())
 # print(Route1.percent_of_route_still_to_travel())
-    path = [[0, 100], [100, 150], [300, 400]]
-    # print(path)
-
-
-    Route1 = Route(path, width=20)
-    print(Route1.routepoints)
 
 
 
+
+# Meine alte Klasse
+
+# import numpy
+# import mathe
+#
+# class Strecke:
+#     """
+#     Eine Strecke ist eine Ansammlung von minimal notwendigen Punkte zum beschreiben einer Route. Der erste Punkt ist der Startpunkt, der letzte Punkt ist der Endpunkt alle anderen Punkte sind Verbindungspunkte die NICHT exakt auf der Route liegen müssen sonderen ggf. daneben
+#     """
+#     def __init__(self):
+#         self.points=[]
+#
+#     def __init__(self, points):
+#         self.points=points
+#
+#     def getStartPoint(self):
+#         return self.points[0]
+#
+#     def getEndPoint(self):
+#         return self.points[-1]
+#
+#     def getSmallestX(self):
+#         x_min=self.points[0].x
+#         for p in self.points:
+#             if p.x<x_min:
+#                 x_min=p.x
+#         return x_min
+#
+#     def getBiggestX(self):
+#         x_max=self.points[0].x
+#         for p in self.points:
+#             if p.x>x_max:
+#                 x_max=p.x
+#         return x_max
+#
+# class Route:
+#     def __init__(self, strecke, dStep, start_pos=None):
+#         self.strecke = strecke # Die Route bassiert auf einer Strecke. Wenn man die Route ändert so ändert man auch die Strecke und umgekehert !
+#         self.points = self._get_path_as_many_points(dStep)
+#         if start_pos==None:
+#             self.positon = strecke.getStartPoint()
+#         else:
+#             self.positon= start_pos
+#         self._dStep=dStep
+#         self.point_iterator = 0 # Position an der wir uns im point "array" befinden
+#
+#     def _get_path_as_many_points(self, dStep): #WARNING EXPENSIVE !
+#         """
+#         Gibt die Route als Punktliste zurück. dStep gibt die Punktgenauigkeit an.
+#         :param dStep: Punkt genauigkeit
+#         :return:
+#         """
+#         roh_daten = self.strecke.points
+#         x_roh=[]
+#         y_roh=[]
+#         for p in roh_daten:
+#             x_roh.append(p.x)
+#             y_roh.append(p.y)
+#         x_range = numpy.arange(self.strecke.getSmallestX(), self.strecke.getBiggestX(), dStep);
+#         y_points = numpy.interp(x_range, x_roh, y_roh)
+#         points=[]
+#         for i in range(0,len(y_points)):
+#         #    points.append(mathe.Point(y_points[i],x_range[i]))
+#             points.append(mathe.Point(x_range[i], y_points[i]))
+#
+#         return points
+#
+#     def get_new_pos(self, l): # ich gehe davon aus das sich die Einheit nicht ändert !
+#         """Verändert die Position um den Abstand l. l ist t*v"""
+#         k=l/self._dStep
+#         self.point_iterator+=int(k)
+#         if self.point_iterator >= len(self.points):
+#             self.point_iterator= len(self.points)-1
+#         return self.points[self.point_iterator]
+#
+#     def get_current_pos(self):
+#         return self.points[self.point_iterator]
+#
+#     def traveled_distance_on_route(self):
+#         """"Zurückgelegter Weg auf der Route d.h. wie weit wir schon gefahren sind"""
+#         k = 1 / self._dStep
+#         return self.point_iterator*k
+#
+#     def percent_of_route_still_to_travel(self):
+#         """Wie viel Prozent der Route noch zurückgelegt werden müssen wobei 0 Prozent heißt das wir angekommen sind und 100 Prozent das wir am Start sind"""
+#         return 100-(100*self.point_iterator/(len(self.points)-1))
+#
+#     def get_angle_of_pos(self, pos):
+#         """Gibt den Winkel zurück so als ob das Auto von Start zu Ende geht"""
+#     #    return angle
+#
+# # myStrecke = Strecke([mathe.Point(0.0,0.0),mathe.Point(1.0,2.0),mathe.Point(2.0,4.0),mathe.Point(3.0,8.0)])
+# # myRoute = Route(myStrecke,0.1)
+# # print(myRoute.get_current_pos())
+# # print(myRoute.percent_of_route_still_to_travel())
+# # print(myRoute.get_new_pos(1))
+# # print(myRoute.percent_of_route_still_to_travel())
+# # print(myRoute.get_new_pos(1))
+# # print(myRoute.percent_of_route_still_to_travel())
+# # print(myRoute.get_new_pos(1))
+# # print(myRoute.percent_of_route_still_to_travel())
